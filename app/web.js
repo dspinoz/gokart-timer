caf1ffec6bdc750bf69d415e8232132210884e40
Daniel <dspinoz@gmail.com>
2015-09-09 00:01:59 +0100

    setup websocket to client

--- app/web.js
+++ app/web.js
@@ -1,6 +1,8 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 var express = require('express');
-var request = require('request');
 var app     = express();
+var server  = http.createServer(app);
 
 var config = require('../config/gokart-timer');
 var routes = require('./gokart-timer');
@@ -10,7 +12,7 @@ app.configure(function() {
   app.use(express.logger('dev')); // log every request to the console
 
   app.use("/css",  express.static(__dirname + '/../views/css'));
-app.use("/js", express.static(__dirname + '/../views/js'));
+  app.use("/js", express.static(__dirname + '/../views/js'));
 
   //pretty print templated resources
   app.locals.pretty = true;
@@ -24,8 +26,52 @@ routes(app);
 console.log("Web starting...");
 exports = module.exports = app;
 
-app.listen(config.port, function() { // startup our app at http://localhost:port
+server.listen(config.port, function() { // startup our app at http://localhost:port
   console.log("Web ok! Listening on " + config.port + "...");
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin 
+      request.reject();
+      console.log('Connection from', request.origin, 'rejected!');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log('Connection from', request.origin, 'accepted.');
+
+    connection.send(JSON.stringify({message: 'hello'}));
+
+    connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        console.log('Received Message from', connection.remoteAddress, ':', message.utf8Data);
+
+        var js = JSON.parse(message.utf8Data);
+        connection.sendUTF(JSON.stringify({message: js.txt}));
+      }
+      else if (message.type === 'binary') {
+        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        connection.sendBytes(message.binaryData);
+      }
+    });
+    connection.on('close', function(reasonCode, description) {
+      console.log('Connection', connection.remoteAddress, 'disconnected.');
+    });
 });
 
 process.on('SIGINT', function() {
--- views/js/index.js
+++ views/js/index.js
@@ -1,1 +1,18 @@
-alert('aaa');
+var conn = new WebSocket(document.URL.replace('http','ws'), "echo-protocol");
+
+conn.onopen = function() {
+  console.log('connected');
+};
+
+conn.onerror = function(err) {
+  console.log('connection error', err);
+};
+
+conn.onmessage = function(msg) {
+  console.log('message', msg);
+};
+
+conn.onclose = function() {
+  console.log('disconnected');
+};
+
