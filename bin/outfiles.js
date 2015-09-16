0958d7ccac272f208e55918ec78113b16d3f0f34
daniel <dspinoz@gmail.com>
2015-09-16 16:55:46 +0930

    support conversion from GPX files

--- bin/outfiles.js
+++ bin/outfiles.js
@@ -2,10 +2,15 @@
 
 var fs = require('fs');
 var cli = require('commander');
+var xmldoc = require('xmldoc');
 
 var cmd = { };
 
-cli.version('0.0.1')
+cli.version('0.0.1');
+
+cli.command('concat')
+  .description('concat all files into one')
+  .option('-o, --output <file>', 'specify output filename', 'all.out')
   .option('-p, --path <path>', 'change the input path', process.cwd())
   .option('-f, --format <regex>', 'change the file name format. must specify at least one regex group for sorting purposes', function(arg) {
     console.log('format', arg);
@@ -15,55 +20,93 @@ cli.version('0.0.1')
       return arg;
     }
     return false;
-  }, "^gps-([0-9]*).out$");
-
-cli.command('concat')
-  .description('concat all files into one')
-  .option('-o, --output <file>', 'specify output filename', 'all.out')
+  }, "^gps-([0-9]*).out$")
   .action(function(opt) {
-    cmd.concat = { output: opt.output};
+    cmd.concat = { output: opt.output, path: opt.path, format: opt.format};
   });
 
+cli.command('convert')
+  .description('convert files from another format')
+  .option('-o, --output <file>', 'specify output filename', 'convert.out')
+  .option('--gpx <file>', 'specify input filename in GPX (GPS Exchange Format)', 'data.gpx')
+  .action(function(opt) {
+    cmd.convert = { output: opt.output, gpx: opt.gpx };
+  });
+  
 cli.parse(process.argv);
 
+if (cmd.concat) {
 
-if (!cli.format) {
-  console.error("Invalid format regex");
-  process.exit(1);
+  if (!cmd.concat.format) {
+    console.error("Invalid format regex");
+    process.exit(1);
+  }
+  
+  var matcher = new RegExp(cmd.concat.format);
+
+  var files = fs.readdirSync(cmd.concat.path)
+    .filter(function(f) {
+      return matcher.test(f);
+    })
+    .sort(function(a,b) {
+      var resA = matcher.exec(a);
+      var resB = matcher.exec(b);
+      return resA[1] - resB[1];
+    });
+    
+  console.log('Got', files.length, 'files that match');
+
+  if (files.length > 0) {
+    console.log('Running concat');
+    var out = [];
+    files.forEach(function(f) {
+      var b = fs.readFileSync(cmd.concat.path + '/' + f);
+
+      if (b && b.length) {
+
+        var json = JSON.parse(b.toString());
+        json.forEach(function(pt) {
+          out.push(pt);
+        });
+      }
+    });
+    
+    var json = JSON.stringify(out, true, ' ');
+    console.log('Writing', json.length, 'bytes to file', cmd.concat.output);
+    fs.writeFileSync(cmd.concat.output, json);
+    console.log('done!');
+  }
 }
+else if (cmd.convert) {
 
+  var data = [];
 
-var matcher = new RegExp(cli.format);
-
-var files = fs.readdirSync(cli.path)
-  .filter(function(f) {
-    return matcher.test(f);
-  })
-  .sort(function(a,b) {
-    var resA = matcher.exec(a);
-    var resB = matcher.exec(b);
-    return resA[1] - resB[1];
-  });
-  
-console.log('Got', files.length, 'files that match');
-
-if (cmd.concat && files.length > 0) {
-  console.log('Running concat');
-  var out = [];
-  files.forEach(function(f) {
-    var b = fs.readFileSync(cli.path + '/' + f);
-
-    if (b && b.length) {
-
-      var json = JSON.parse(b.toString());
-      json.forEach(function(pt) {
-        out.push(pt);
+  if (cmd.convert.gpx) {
+    var gpx = new xmldoc.XmlDocument(fs.readFileSync(cmd.convert.gpx));
+    gpx.childrenNamed('trk').forEach(function(trk) {
+      trk.childrenNamed('trkseg').forEach(function(trkseg) {
+        trkseg.childrenNamed('trkpt').forEach(function(trkpt) {
+          var d = {lat: +trkpt.attr.lat, 
+                   lon: +trkpt.attr.lon, 
+                   time: trkpt.childNamed('time').val};
+          
+          data.push(d);
+        });
       });
-    }
-  });
+    });
+  }
+  else {
+    console.err('Unknown conversion format specified');
+    process.exit(1);
+  }
   
-  var json = JSON.stringify(out, true, ' ');
-  console.log('Writing', json.length, 'bytes to file', cmd.concat.output);
-  fs.writeFileSync(cmd.concat.output, json);
-  console.log('done!');
+  console.log('Found', data.length, 'data points from input');
+  if (data.length > 0) {
+  
+    var json = JSON.stringify(data, true, ' ');
+    console.log('Writing', json.length, 'bytes to file', cmd.convert.output);
+    fs.writeFileSync(cmd.convert.output, json);
+    console.log('done!');
+  }
+  
 }
